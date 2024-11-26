36964ca49708068fff15cfca145ebc78c5eb854f
Michael Connaker <git@connaker.org>
2024-11-26 08:44:00 -0600

    Adding healtcheck.py, healthcheck.yaml and requirements file. Updating README.MD file.

--- README.md
+++ README.md
@@ -1,5 +1,51 @@
-# http_endpoints_healthcheck
+# HTTP Endpoint Healthcheck
 
 ## About this Project
 
 > This program checks the health of a set of HTTP endpoints. It reads an input argument from a file path with a list of HTTP endpoints in YAML format, tests the health of the endpoints in 15 second test cycles and displays results of the availability percentage of the HTTP root domains being monitored by the program.
+
+## Built with
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+
+## Getting Started
+
+### Prerequisites
+
+>The following are the prequisites to run this program. It is assumed that these are configured prior to running the program.
+
+- IDE (such as VsCode)
+- Python 3.12.6
+- virtual environment
+
+### Installation
+
+1. clone the repo
+
+   ```sh
+   git clone https://github.com/Connaker/http_endpoints_healthcheck.git
+   ```
+
+2. Start your virtual environment
+
+   ```sh
+   macOS: source .\venv\Scripts\activate <br>
+   Windows: .\venv\Scripts\activate
+   ```
+
+3. Install requiremetns.txt
+
+   ```sh
+   pip install -r requirements.txt
+   ```
+
+### Update healthcheck.yaml
+
+>In order for the code to run, you must update the url, name and method healthcheck.yaml file. Method needs to be either GET or POST. For POST add in any headers (example:(username, password)) or body (example:("foo":"bar")) to send.
+
+### Usage
+
+> to run the program, use `python healtcheck.py`. The program will continue to run until you stop it by using `CTRL+C` at which time the program will exit.
+
+### Notes
+> The program is designed to automatically fail any HTTP endpoints with a millisecond greater than 500. It is designed to send both Headers and Data(Body).
---
+++ healthcheck.py
@@ -0,0 +1,98 @@
+import requests
+import yaml
+import time
+from urllib.parse import urlparse
+import math
+
+def file_check(file_path):
+    with open(file_path, 'r') as file:
+        data = yaml.safe_load(file)
+
+        if not data or 'resources' not in data:
+            raise ValueError("Invalid file format")
+        
+        check = []
+        for resource in data['resources']:
+            url = resource.get('url')
+            headers = []
+            headers = resource.get('headers')
+            method = resource.get('method')
+            body = resource.get('body')
+            check.append({
+                'url': url,
+                'headers': headers,
+                'method': method,
+                'body': body,
+            })
+
+    endpoint_healthcheck(check)
+
+def endpoint_healthcheck(urls):
+    cycle_count = 0
+    start_time = 0 
+    data = []
+
+    while True:
+        print(f"Test cycle #{cycle_count + 1} at time = {start_time} seconds")
+        for item in urls:
+            method = item['method']
+            up_count=0
+            try:
+                if method == 'POST':
+                    response = requests.post(item['url'], data=item['body'], headers=item['headers'])
+                else:
+                    response = requests.get(item['url'], data=item['body'], headers=item['headers'])
+
+                if response and response.status_code // 100 == 2:
+                    mseconds_check = response.elapsed.total_seconds() * 1000
+
+                    if mseconds_check > 600:
+                        print(f"Endpoint with the name {item['url']} has HTTP response code {response.status_code} and the response latency {math.ceil(mseconds_check)} => DOWN")
+                    else:
+                        up_count +=1
+                        print(f"Endpoint with the name {item['url']} has HTTP response code {response.status_code} and the response latency {math.ceil(mseconds_check)} => UP")        
+
+                else:
+                    print(f"Endpoint with the name {item['url']} has HTTP response code {response.status_code} and the response latency {math.ceil(mseconds_check)} => DOWN")
+
+
+            except requests.RequestException as e:
+                print(f"Error checking {item['url']}: {e}")
+            data.append({
+                "url": item['url'],
+                "up_count": up_count,
+            })
+
+        cycle_count +=1
+        print(f"Test cycle #{cycle_count} ends. The program logs to the console:")
+        health_check_results(data)
+        start_time += 15
+
+def health_check_results(data):
+    combined_results={}
+    
+    for item in data:
+        base_url = get_base_url(item["url"])
+        up_count = item["up_count"]
+
+        if base_url in combined_results:
+            combined_results[base_url]["up_count"] += up_count
+            combined_results[base_url]["count"] += 1
+           
+        else:
+            combined_results[base_url] = {"up_count": up_count, "count": 1}
+
+    for key, value in combined_results.items():
+        uc = value['up_count']
+        c = value['count']
+        totals = 100 * uc / c
+        
+        print(f"{base_url} has {math.ceil(totals)} availability percentage")
+    time.sleep(15)
+
+def get_base_url(url):
+    parsed = urlparse(url)
+    return f"{parsed.scheme}://{parsed.netloc}"
+
+file_path = 'local_healthcheck.yaml'
+config_data = file_check(file_path)
